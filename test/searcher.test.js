5274f1a9e6b1cbfe267b32911febcf2beb75b894
Jonathan Poelen <jonathan.poelen@gmail.com>
2022-11-26 17:57:54 +0100

    rename foundLength to lengthFound

--- test/searcher.test.js
+++ test/searcher.test.js
@@ -84,6 +84,7 @@ test.each([
   expect(searcher.createFuzzySearcher(options, c)(si)).toBe(expectedIndex);
 });
 
+
 // plainText
 test.each([
   // lastIndex = -1
@@ -150,71 +151,71 @@ test.each([
   [UPPERCASE_ONLY | ACCENT_ONLY, {str:'aéb', lastIndex: -1}, 'É', -1, 1],
   [UPPERCASE_ONLY | ACCENT_ONLY, {str:'AÉB', lastIndex: -1}, 'É', 1, 1],
 
-  // lastIndex = 0, foundLength: 1
-  [CASE_SENSITIVE | ACCENT_SENSITIVE, {str:'abcabc', lastIndex: 0, foundLength: 1}, 'a', -1, 2],
-  [CASE_SENSITIVE | ACCENT_SENSITIVE, {str:'abcabc', lastIndex: 0, foundLength: 1}, 'b', 0, 2],
-  [CASE_SENSITIVE | ACCENT_SENSITIVE, {str:'abcabc', lastIndex: 0, foundLength: 1}, 'bc', 0, 3],
+  // lastIndex = 0, lengthFound: 1
+  [CASE_SENSITIVE | ACCENT_SENSITIVE, {str:'abcabc', lastIndex: 0, lengthFound: 1}, 'a', -1, 2],
+  [CASE_SENSITIVE | ACCENT_SENSITIVE, {str:'abcabc', lastIndex: 0, lengthFound: 1}, 'b', 0, 2],
+  [CASE_SENSITIVE | ACCENT_SENSITIVE, {str:'abcabc', lastIndex: 0, lengthFound: 1}, 'bc', 0, 3],
 
-  [CASE_INSENSITIVE | ACCENT_SENSITIVE, {upperStr:'ABCABC', lastIndex: 0, foundLength: 1}, 'B', 0, 2],
-  [CASE_INSENSITIVE | ACCENT_SENSITIVE, {upperStr:'ABCABC', lastIndex: 0, foundLength: 1}, 'b', 0, 2],
-  [CASE_INSENSITIVE | ACCENT_SENSITIVE, {upperStr:'ABCABC', lastIndex: 0, foundLength: 1}, 'd', -1, 2],
+  [CASE_INSENSITIVE | ACCENT_SENSITIVE, {upperStr:'ABCABC', lastIndex: 0, lengthFound: 1}, 'B', 0, 2],
+  [CASE_INSENSITIVE | ACCENT_SENSITIVE, {upperStr:'ABCABC', lastIndex: 0, lengthFound: 1}, 'b', 0, 2],
+  [CASE_INSENSITIVE | ACCENT_SENSITIVE, {upperStr:'ABCABC', lastIndex: 0, lengthFound: 1}, 'd', -1, 2],
 
-  [CASE_SENSITIVE | ACCENT_INSENSITIVE, {unaccentedStr:'aeb', lastIndex: 0, foundLength: 1}, 'e', 0, 2],
-  [CASE_SENSITIVE | ACCENT_INSENSITIVE, {unaccentedStr:'aeb', lastIndex: 0, foundLength: 1}, 'é', 0, 2],
-  [CASE_SENSITIVE | ACCENT_INSENSITIVE, {unaccentedStr:'aEb', lastIndex: 0, foundLength: 1}, 'é', -1, 2],
+  [CASE_SENSITIVE | ACCENT_INSENSITIVE, {unaccentedStr:'aeb', lastIndex: 0, lengthFound: 1}, 'e', 0, 2],
+  [CASE_SENSITIVE | ACCENT_INSENSITIVE, {unaccentedStr:'aeb', lastIndex: 0, lengthFound: 1}, 'é', 0, 2],
+  [CASE_SENSITIVE | ACCENT_INSENSITIVE, {unaccentedStr:'aEb', lastIndex: 0, lengthFound: 1}, 'é', -1, 2],
 
-  [CASE_INSENSITIVE | ACCENT_INSENSITIVE, {unaccentedUpperStr:'AEB', lastIndex: 0, foundLength: 1}, 'e', 0, 2],
-  [CASE_INSENSITIVE | ACCENT_INSENSITIVE, {unaccentedUpperStr:'AEB', lastIndex: 0, foundLength: 1}, 'é', 0, 2],
-  [CASE_INSENSITIVE | ACCENT_INSENSITIVE, {unaccentedUpperStr:'AEB', lastIndex: 0, foundLength: 1}, 'E', 0, 2],
-  [CASE_INSENSITIVE | ACCENT_INSENSITIVE, {unaccentedUpperStr:'AEB', lastIndex: 0, foundLength: 1}, 'É', 0, 2],
-  [CASE_INSENSITIVE | ACCENT_INSENSITIVE, {unaccentedUpperStr:'AEB', lastIndex: 0, foundLength: 1}, 'd', -1, 2],
+  [CASE_INSENSITIVE | ACCENT_INSENSITIVE, {unaccentedUpperStr:'AEB', lastIndex: 0, lengthFound: 1}, 'e', 0, 2],
+  [CASE_INSENSITIVE | ACCENT_INSENSITIVE, {unaccentedUpperStr:'AEB', lastIndex: 0, lengthFound: 1}, 'é', 0, 2],
+  [CASE_INSENSITIVE | ACCENT_INSENSITIVE, {unaccentedUpperStr:'AEB', lastIndex: 0, lengthFound: 1}, 'E', 0, 2],
+  [CASE_INSENSITIVE | ACCENT_INSENSITIVE, {unaccentedUpperStr:'AEB', lastIndex: 0, lengthFound: 1}, 'É', 0, 2],
+  [CASE_INSENSITIVE | ACCENT_INSENSITIVE, {unaccentedUpperStr:'AEB', lastIndex: 0, lengthFound: 1}, 'd', -1, 2],
 
-  [CASE_SENSITIVE | ACCENT_ONLY, {unaccentedStr:'aeb', lastIndex: 0, foundLength: 1}, 'e', 0, 2],
-  [CASE_SENSITIVE | ACCENT_ONLY, {unaccentedStr:'aEb', lastIndex: 0, foundLength: 1}, 'e', -1, 2],
-  [CASE_SENSITIVE | ACCENT_ONLY, {unaccentedStr:'aeb', lastIndex: 0, foundLength: 1}, 'E', -1, 2],
-  [CASE_SENSITIVE | ACCENT_ONLY, {unaccentedStr:'aEb', lastIndex: 0, foundLength: 1}, 'E', 0, 2],
+  [CASE_SENSITIVE | ACCENT_ONLY, {unaccentedStr:'aeb', lastIndex: 0, lengthFound: 1}, 'e', 0, 2],
+  [CASE_SENSITIVE | ACCENT_ONLY, {unaccentedStr:'aEb', lastIndex: 0, lengthFound: 1}, 'e', -1, 2],
+  [CASE_SENSITIVE | ACCENT_ONLY, {unaccentedStr:'aeb', lastIndex: 0, lengthFound: 1}, 'E', -1, 2],
+  [CASE_SENSITIVE | ACCENT_ONLY, {unaccentedStr:'aEb', lastIndex: 0, lengthFound: 1}, 'E', 0, 2],
 
-  [CASE_SENSITIVE | ACCENT_ONLY, {str:'aeb', lastIndex: 0, foundLength: 1}, 'é', -1, 2],
-  [CASE_SENSITIVE | ACCENT_ONLY, {str:'aéb', lastIndex: 0, foundLength: 1}, 'é', 0, 2],
-  [CASE_SENSITIVE | ACCENT_ONLY, {str:'AEB', lastIndex: 0, foundLength: 1}, 'é', -1, 2],
-  [CASE_SENSITIVE | ACCENT_ONLY, {str:'AÉB', lastIndex: 0, foundLength: 1}, 'é', -1, 2],
+  [CASE_SENSITIVE | ACCENT_ONLY, {str:'aeb', lastIndex: 0, lengthFound: 1}, 'é', -1, 2],
+  [CASE_SENSITIVE | ACCENT_ONLY, {str:'aéb', lastIndex: 0, lengthFound: 1}, 'é', 0, 2],
+  [CASE_SENSITIVE | ACCENT_ONLY, {str:'AEB', lastIndex: 0, lengthFound: 1}, 'é', -1, 2],
+  [CASE_SENSITIVE | ACCENT_ONLY, {str:'AÉB', lastIndex: 0, lengthFound: 1}, 'é', -1, 2],
 
-  [CASE_SENSITIVE | ACCENT_ONLY, {str:'aeb', lastIndex: 0, foundLength: 1}, 'É', -1, 2],
-  [CASE_SENSITIVE | ACCENT_ONLY, {str:'aéb', lastIndex: 0, foundLength: 1}, 'É', -1, 2],
-  [CASE_SENSITIVE | ACCENT_ONLY, {str:'AEB', lastIndex: 0, foundLength: 1}, 'É', -1, 2],
-  [CASE_SENSITIVE | ACCENT_ONLY, {str:'AÉB', lastIndex: 0, foundLength: 1}, 'É', 0, 2],
+  [CASE_SENSITIVE | ACCENT_ONLY, {str:'aeb', lastIndex: 0, lengthFound: 1}, 'É', -1, 2],
+  [CASE_SENSITIVE | ACCENT_ONLY, {str:'aéb', lastIndex: 0, lengthFound: 1}, 'É', -1, 2],
+  [CASE_SENSITIVE | ACCENT_ONLY, {str:'AEB', lastIndex: 0, lengthFound: 1}, 'É', -1, 2],
+  [CASE_SENSITIVE | ACCENT_ONLY, {str:'AÉB', lastIndex: 0, lengthFound: 1}, 'É', 0, 2],
 
-  [UPPERCASE_ONLY | ACCENT_SENSITIVE, {upperStr:'AEB', lastIndex: 0, foundLength: 1}, 'e', 0, 2],
-  [UPPERCASE_ONLY | ACCENT_SENSITIVE, {upperStr:'AÉB', lastIndex: 0, foundLength: 1}, 'e', -1, 2],
-  [UPPERCASE_ONLY | ACCENT_SENSITIVE, {upperStr:'AEB', lastIndex: 0, foundLength: 1}, 'é', -1, 2],
-  [UPPERCASE_ONLY | ACCENT_SENSITIVE, {upperStr:'AÉB', lastIndex: 0, foundLength: 1}, 'é', 0, 2],
+  [UPPERCASE_ONLY | ACCENT_SENSITIVE, {upperStr:'AEB', lastIndex: 0, lengthFound: 1}, 'e', 0, 2],
+  [UPPERCASE_ONLY | ACCENT_SENSITIVE, {upperStr:'AÉB', lastIndex: 0, lengthFound: 1}, 'e', -1, 2],
+  [UPPERCASE_ONLY | ACCENT_SENSITIVE, {upperStr:'AEB', lastIndex: 0, lengthFound: 1}, 'é', -1, 2],
+  [UPPERCASE_ONLY | ACCENT_SENSITIVE, {upperStr:'AÉB', lastIndex: 0, lengthFound: 1}, 'é', 0, 2],
 
-  [UPPERCASE_ONLY | ACCENT_SENSITIVE, {str:'aeb', lastIndex: 0, foundLength: 1}, 'E', -1, 2],
-  [UPPERCASE_ONLY | ACCENT_SENSITIVE, {str:'aéb', lastIndex: 0, foundLength: 1}, 'E', -1, 2],
-  [UPPERCASE_ONLY | ACCENT_SENSITIVE, {str:'AEB', lastIndex: 0, foundLength: 1}, 'E', 0, 2],
-  [UPPERCASE_ONLY | ACCENT_SENSITIVE, {str:'AÉB', lastIndex: 0, foundLength: 1}, 'E', -1, 2],
+  [UPPERCASE_ONLY | ACCENT_SENSITIVE, {str:'aeb', lastIndex: 0, lengthFound: 1}, 'E', -1, 2],
+  [UPPERCASE_ONLY | ACCENT_SENSITIVE, {str:'aéb', lastIndex: 0, lengthFound: 1}, 'E', -1, 2],
+  [UPPERCASE_ONLY | ACCENT_SENSITIVE, {str:'AEB', lastIndex: 0, lengthFound: 1}, 'E', 0, 2],
+  [UPPERCASE_ONLY | ACCENT_SENSITIVE, {str:'AÉB', lastIndex: 0, lengthFound: 1}, 'E', -1, 2],
 
-  [UPPERCASE_ONLY | ACCENT_SENSITIVE, {str:'aeb', lastIndex: 0, foundLength: 1}, 'É', -1, 2],
-  [UPPERCASE_ONLY | ACCENT_SENSITIVE, {str:'aéb', lastIndex: 0, foundLength: 1}, 'É', -1, 2],
-  [UPPERCASE_ONLY | ACCENT_SENSITIVE, {str:'AEB', lastIndex: 0, foundLength: 1}, 'É', -1, 2],
-  [UPPERCASE_ONLY | ACCENT_SENSITIVE, {str:'AÉB', lastIndex: 0, foundLength: 1}, 'É', 0, 2],
+  [UPPERCASE_ONLY | ACCENT_SENSITIVE, {str:'aeb', lastIndex: 0, lengthFound: 1}, 'É', -1, 2],
+  [UPPERCASE_ONLY | ACCENT_SENSITIVE, {str:'aéb', lastIndex: 0, lengthFound: 1}, 'É', -1, 2],
+  [UPPERCASE_ONLY | ACCENT_SENSITIVE, {str:'AEB', lastIndex: 0, lengthFound: 1}, 'É', -1, 2],
+  [UPPERCASE_ONLY | ACCENT_SENSITIVE, {str:'AÉB', lastIndex: 0, lengthFound: 1}, 'É', 0, 2],
 
-  [UPPERCASE_ONLY | ACCENT_ONLY, {unaccentedUpperStr:'AEB', lastIndex: 0, foundLength: 1}, 'e', 0, 2],
+  [UPPERCASE_ONLY | ACCENT_ONLY, {unaccentedUpperStr:'AEB', lastIndex: 0, lengthFound: 1}, 'e', 0, 2],
 
-  [UPPERCASE_ONLY | ACCENT_ONLY, {unaccentedStr:'aeb', lastIndex: 0, foundLength: 1}, 'E', -1, 2],
-  [UPPERCASE_ONLY | ACCENT_ONLY, {unaccentedStr:'AEB', lastIndex: 0, foundLength: 1}, 'E', 0, 2],
+  [UPPERCASE_ONLY | ACCENT_ONLY, {unaccentedStr:'aeb', lastIndex: 0, lengthFound: 1}, 'E', -1, 2],
+  [UPPERCASE_ONLY | ACCENT_ONLY, {unaccentedStr:'AEB', lastIndex: 0, lengthFound: 1}, 'E', 0, 2],
 
-  [UPPERCASE_ONLY | ACCENT_ONLY, {upperStr:'AEB', lastIndex: 0, foundLength: 1}, 'é', -1, 2],
-  [UPPERCASE_ONLY | ACCENT_ONLY, {upperStr:'AÉB', lastIndex: 0, foundLength: 1}, 'é', 0, 2],
+  [UPPERCASE_ONLY | ACCENT_ONLY, {upperStr:'AEB', lastIndex: 0, lengthFound: 1}, 'é', -1, 2],
+  [UPPERCASE_ONLY | ACCENT_ONLY, {upperStr:'AÉB', lastIndex: 0, lengthFound: 1}, 'é', 0, 2],
 
-  [UPPERCASE_ONLY | ACCENT_ONLY, {str:'aeb', lastIndex: 0, foundLength: 1}, 'É', -1, 2],
-  [UPPERCASE_ONLY | ACCENT_ONLY, {str:'AEB', lastIndex: 0, foundLength: 1}, 'É', -1, 2],
-  [UPPERCASE_ONLY | ACCENT_ONLY, {str:'aéb', lastIndex: 0, foundLength: 1}, 'É', -1, 2],
-  [UPPERCASE_ONLY | ACCENT_ONLY, {str:'AÉB', lastIndex: 0, foundLength: 1}, 'É', 0, 2],
+  [UPPERCASE_ONLY | ACCENT_ONLY, {str:'aeb', lastIndex: 0, lengthFound: 1}, 'É', -1, 2],
+  [UPPERCASE_ONLY | ACCENT_ONLY, {str:'AEB', lastIndex: 0, lengthFound: 1}, 'É', -1, 2],
+  [UPPERCASE_ONLY | ACCENT_ONLY, {str:'aéb', lastIndex: 0, lengthFound: 1}, 'É', -1, 2],
+  [UPPERCASE_ONLY | ACCENT_ONLY, {str:'AÉB', lastIndex: 0, lengthFound: 1}, 'É', 0, 2],
 
 ])("plainText(mode=%i, %s, s='%s') = {index:%i, length:%i}", (options, si, str, expectedIndex, expectedLen) => {
   expect(searcher.createTextSearcher(options, str, si.lastIndex !== -1)(si)).toBe(expectedIndex);
-  expect(si.foundLength).toBe(expectedLen);
+  expect(si.lengthFound).toBe(expectedLen);
 });
 
 
@@ -244,7 +245,7 @@ test.each([
 
 ])("regex(mode=%i, %s, s='%s') = {index:%i, length:%i}", (options, si, str, expectedIndex, expectedLen) => {
   expect(searcher.createRegexSearcher(options, str)(si)).toBe(expectedIndex);
-  expect(si.foundLength).toBe(expectedLen);
+  expect(si.lengthFound).toBe(expectedLen);
 });
 
 test('filter()', () => {
